0fea98b2cb0704e918976294875cb347a2ec293e
Elisa <elisadiazartcontact@gmail.com>
2023-01-30 23:02:18 +0100

    30.01.23

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,24 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
-import { sayHello } from './demo.js';
+import { THEMES } from './constants';
 
-sayHello();
+const selectElement= document.getElementById('themes')
+const listElement = document.getElementById('list-container')
+
+const printList = (selectedOption)=> {
+    listElement.innerHTML= ''
+    if(selectedOption === 'default') return
+    const fragment = document.createDocumentFragment()
+    THEMES[selectedOption].forEach(element => {
+        const newLi = document.createElement('li')
+        newLi.textContent= element
+        fragment.append(newLi)
+    });
+    listElement.append(fragment)
+}
+
+selectElement.addEventListener('change', e => {
+    printList(e.target.value)
+})
+
+
